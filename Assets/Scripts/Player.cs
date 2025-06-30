f1d0628eece44ccfb7eac6960df61d23dadc2e78
Dinh Tan Hieu <dinhtanhieu2005@gmail.com>
2025-06-30 20:38:04 +0700

    reset inventory

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,49 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class player : MonoBehaviour
 {
-    private Inventory inventory;
-    [SerializeField] private UI_Inventory uiInventory;
-
-    public float speed = 5.0f;
+    public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    private Animator animator;
+    public Vector3 moveInput;
 
-    private void Awake() {
-        inventory = new Inventory();
-        uiInventory.SetInventory(inventory);
-    }
-
-    void Start()
-    {
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(new Vector2(-speed * Time.deltaTime, 0));
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(new Vector2(speed * Time.deltaTime, 0));
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.AddForce(new Vector2(0, speed * Time.deltaTime));
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(new Vector2(0, -speed * Time.deltaTime));
-        }
-    }
+    private void Update() {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
 
-    private void OnTriggerEnter2D(Collider2D collider) {
-        ItemWorld itemworld = collider.GetComponent<ItemWorld>();
-        if (itemworld != null) {
-            inventory.AddItem(itemworld.GetItem());
-            itemworld.DestroySelf();
+        if (moveInput.x != 0) {
+            if (moveInput.x > 0) {
+                transform.localScale = new Vector3(1, 1, 0);
+                animator.SetBool("isRightLeft", true);
+            } else {
+                transform.localScale = new Vector3(-1, 1, 0);
+                animator.SetBool("isRightLeft", true);
+            }
+        }
+        else {
+            animator.SetBool("isRightLeft", false);
+        }
+
+        if (moveInput.y != 0) {
+            if (moveInput.y > 0) {
+                animator.SetBool("isBehind", true);
+            } else {
+                animator.SetBool("isAhead", true);
+            }
+        }
+        else {
+            animator.SetBool("isBehind", false);
+            animator.SetBool("isAhead", false);
         }
     }
 }
